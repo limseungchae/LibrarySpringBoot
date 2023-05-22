a05f69e797683590c5a167b1ca9a568a10a2ebd1
limseungchae <tmdco12@naver.com>
2023-05-22 17:12:58 +0900

    프로젝트 설정 파일 작성 컨트롤러 작성, list.html의 nav 태그 주석처리

---
+++ src/main/java/exam/springboot/libraryspringboot/controller/LibraryController.java
@@ -0,0 +1,15 @@
+package exam.springboot.libraryspringboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibraryController {
+
+    @GetMapping("/")
+    public String list() {
+        return "list";
+    }
+
+}
