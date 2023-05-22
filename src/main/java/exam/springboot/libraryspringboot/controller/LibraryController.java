6e8acc2a15cfb15c2ae33f34a4d5cdc1b7d48783
limseungchae <tmdco12@naver.com>
2023-05-22 17:55:02 +0900

    controller 에서 Library 서비스 호출

--- src/main/java/exam/springboot/libraryspringboot/controller/LibraryController.java
+++ src/main/java/exam/springboot/libraryspringboot/controller/LibraryController.java
@@ -1,14 +1,28 @@
 package exam.springboot.libraryspringboot.controller;
 
-
+import exam.springboot.libraryspringboot.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LibraryController {
 
+
+@Autowired
+LibraryService lbsrv;
+
+
     @GetMapping("/")
-    public String list() {
+    public String list(Integer cpg) {
+        ModelAndView mv = new ModelAndView();
+
+        if (cpg == null || cpg == 0) cpg = 1;
+        mv.addObject("lblist", lbsrv.readLibrary(cpg));
+
+        mv.setViewName("list");
+
         return "list";
     }
 
